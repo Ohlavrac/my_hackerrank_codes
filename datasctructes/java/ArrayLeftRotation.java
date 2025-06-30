b68d8736b3e7434152b95dbad8f623e4df7f4558
ohlavrac <joaovicrcarvalho@gmail.com>
2025-06-30 08:59:59 -0300

    Solved: Array Left Rotation from Data Structures (ARRAY)

---
+++ datasctructes/java/ArrayLeftRotation.java
@@ -0,0 +1,50 @@
+package datasctructes.java;
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static List<Integer> rotateLeft(int d, List<Integer> arr) {
+        int firstValueAux = arr.get(0);
+        
+        for (int index = 0; index < d; index++) {
+            arr.remove(0);
+            arr.add(firstValueAux);
+            firstValueAux = arr.get(0);
+        }
+        return arr; 
+    }
+
+}
+
+public class ArrayLeftRotation {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int d = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = Result.rotateLeft(d, arr);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
